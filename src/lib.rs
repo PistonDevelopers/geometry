640a31c4c8279ac38d4dbe2d30c5f166d764ccdb
Sven Nilsen <bvssvni@gmail.com>
2015-02-13 13:56:15 +0100

    Added `ObjectRange`, made geometry child `u32`

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,25 @@ extern crate range;
 
 use range::{ Range, ParentRange };
 
-/// A geometry range, pointing to triangle list index buffer.
-pub struct GeometryRange<I>(Range);
+/// A geometry range, pointing to triangle list `u32` index buffer.
+pub struct GeometryRange(Range);
 
-impl<I> ParentRange for GeometryRange<I> {
-    type Child = I;
+impl ParentRange for GeometryRange {
+    type Child = u32;
 
     fn from_range(range: Range) -> Self { GeometryRange(range) }
+    fn range(&self) -> &Range { &self.0 }
+    fn range_mut(&mut self) -> &mut Range { &mut self.0 }
+}
+
+/// An object range, pointing to a geometry range buffer,
+/// which points to a triangle list index `I` buffer.
+pub struct ObjectRange(Range);
+
+impl ParentRange for ObjectRange {
+    type Child = GeometryRange;
+
+    fn from_range(range: Range) -> Self { ObjectRange(range) }
     fn range(&self) -> &Range { &self.0 }
     fn range_mut(&mut self) -> &mut Range { &mut self.0 }
 }
