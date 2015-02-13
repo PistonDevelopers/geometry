38ba85ceab9bfc707d12e0d0bcd830741c3a96a0
Sven Nilsen <bvssvni@gmail.com>
2015-02-13 13:59:32 +0100

    Added `ObjectSetRange`

--- src/lib.rs
+++ src/lib.rs
@@ -17,14 +17,24 @@ impl ParentRange for GeometryRange {
     fn range_mut(&mut self) -> &mut Range { &mut self.0 }
 }
 
-/// An object range, pointing to a geometry range buffer,
-/// which points to a triangle list index `I` buffer.
+/// An object range, pointing to a geometry range buffer.
 pub struct ObjectRange(Range);
 
 impl ParentRange for ObjectRange {
     type Child = GeometryRange;
 
     fn from_range(range: Range) -> Self { ObjectRange(range) }
+    fn range(&self) -> &Range { &self.0 }
+    fn range_mut(&mut self) -> &mut Range { &mut self.0 }
+}
+
+/// An object set range, pointing to an object range buffer.
+pub struct ObjectSetRange(Range);
+
+impl ParentRange for ObjectSetRange {
+    type Child = ObjectRange;
+
+    fn from_range(range: Range) -> Self { ObjectSetRange(range) }
     fn range(&self) -> &Range { &self.0 }
     fn range_mut(&mut self) -> &mut Range { &mut self.0 }
 }
