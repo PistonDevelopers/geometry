00981efb51e5b9dd564ac07d1fe4dd0db12b0d36
Sven Nilsen <bvssvni@gmail.com>
2015-02-14 15:24:24 +0100

    Transforming uv and normal indices to vertex
    Closes https://github.com/PistonDevelopers/geometry/issues/19

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 
 extern crate range;
 extern crate "wavefront_obj" as wobj;
+extern crate quack;
 
 pub use geometry::Geometry;
 pub use object::Object;
@@ -12,3 +13,31 @@ pub use model::Model;
 mod geometry;
 mod object;
 mod model;
+
+/// Property type for position.
+pub struct Position(pub [f32; 3]);
+
+/// Property type for texture coordinates.
+pub struct TextureCoords(pub [f32; 2]);
+
+/// Property type for normal.
+pub struct Normal(pub [f32; 3]);
+
+/// Description of vertex format.
+pub enum VertexFormat {
+    /// Vertex contains position coordinates.
+    Position,
+    /// Vertex contains position and texture coordinates.
+    PositionTexture,
+    /// Vertex contains position and normal coordinates.
+    PositionNormal,
+    /// Vertex contains both position, texture and normal coordinates.
+    PositionTextureNormal,
+}
+
+/// An error with vertex format when adding to index and vertex buffer.
+#[derive(Debug)]
+pub enum VertexFormatError {
+    /// The vertex format must be the same for an entire geometry object.
+    ExpectedSameVertexFormatPerGeometry,
+}
