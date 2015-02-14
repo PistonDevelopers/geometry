764a30c0a29e731ac0970d9ccab2ab364166c5de
Sven Nilsen <bvssvni@gmail.com>
2015-02-14 17:27:53 +0100

    Added `Model::add_model`

--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,13 @@
 use range::{ Range, ParentRange };
+use wobj;
+use quack::{ Pair, SetAt };
+use std::default::Default;
 
+use Normal;
+use TextureCoords;
+use Position;
 use Object;
+use Geometry;
 
 /// A model consists of a list of object.
 /// The objects are stored separately,
@@ -13,4 +20,33 @@ impl ParentRange for Model {
     fn from_range(range: Range) -> Self { Model(range) }
     fn range(&self) -> &Range { &self.0 }
     fn range_mut(&mut self) -> &mut Range { &mut self.0 }
+}
+
+impl Model {
+    /// Adds a model.
+    pub fn add_model<T>(
+        obj_set: &wobj::obj::ObjSet,
+        vertices: &mut Vec<T>,
+        indices: &mut Vec<u32>,
+        geometries: &mut Vec<Geometry>,
+        objects: &mut Vec<Object>,
+    ) -> Model
+        where
+            T: Default,
+            (Position, T): Pair<Data = Position, Object = T> + SetAt,
+            (TextureCoords, T): Pair<Data = TextureCoords, Object = T> + SetAt,
+            (Normal, T): Pair<Data = Normal, Object = T> + SetAt
+    {
+        let start = objects.len();
+        for obj in obj_set.objects.iter() {
+            objects.push(Object::add_object(
+                obj,
+                vertices,
+                indices,
+                geometries
+            ));
+        }
+        let n = objects.len() - start;
+        Model(Range::new(start, n))
+    }
 }
