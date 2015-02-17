73eb205a3788224415600d7895764aa1185f9b82
Sven Nilsen <bvssvni@gmail.com>
2015-02-17 20:14:41 +0100

    Update to latest piston-range

--- src/model.rs
+++ src/model.rs
@@ -1,4 +1,4 @@
-use range::{ Range, ParentRange };
+use range::{ AddTo, Range };
 use wobj;
 use quack::{ Pair, SetAt };
 use std::default::Default;
@@ -12,15 +12,7 @@ use Geometry;
 /// A model consists of a list of object.
 /// The objects are stored separately,
 /// in an object buffer.
-pub struct Model(Range);
-
-impl ParentRange for Model {
-    type Child = Object;
-
-    fn from_range(range: Range) -> Self { Model(range) }
-    fn range(&self) -> &Range { &self.0 }
-    fn range_mut(&mut self) -> &mut Range { &mut self.0 }
-}
+pub struct Model(pub Vec<Range>);
 
 impl Model {
     /// Adds a model.
@@ -28,16 +20,16 @@ impl Model {
         obj_set: &wobj::obj::ObjSet,
         vertices: &mut Vec<T>,
         indices: &mut Vec<u32>,
-        geometries: &mut Vec<Geometry>,
-        objects: &mut Vec<Object>,
-    ) -> Model
+        geometries: &mut Geometry,
+        objects: &mut Object,
+    ) -> Range<AddTo<Model>>
         where
             T: Default,
             (Position, T): Pair<Data = Position, Object = T> + SetAt,
             (TextureCoords, T): Pair<Data = TextureCoords, Object = T> + SetAt,
             (Normal, T): Pair<Data = Normal, Object = T> + SetAt
     {
-        let start = objects.len();
+        let start = objects.0.len();
         for obj in obj_set.objects.iter() {
             objects.push(Object::add_object(
                 obj,
@@ -46,7 +38,7 @@ impl Model {
                 geometries
             ));
         }
-        let n = objects.len() - start;
-        Model(Range::new(start, n))
+        let n = objects.0.len() - start;
+        Range::new(start, n)
     }
 }
